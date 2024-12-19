450fc616cc90cb186e23c262dfd12178ac8282a9
khoitongdz <khoitong0000@gmail.com>
2024-12-19 17:47:39 +0700

    Update kenon.lua

--- kenon.lua
+++ kenon.lua
@@ -3,7 +3,7 @@ local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shle
 local Window = OrionLib:MakeWindow({Name = "𝕜𝕖𝕟𝕠𝕟 𝕙𝕦𝕓", HidePremium = false, SaveConfig = true, ConfigFolder = "𝕜𝕖𝕟𝕠𝕟 𝕙𝕦𝕓"})
 
 -- Tải toàn bộ tính năng từ link bạn cung cấp
-local bananaScript = loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-BananaCat.lua"))()
+local bananaScript = loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
 
 -- Khởi động giao diện Orion
 OrionLib:Init()
