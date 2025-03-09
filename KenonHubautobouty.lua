a71d7d0676b7e70dce4c9466b0d68e8b981b2a53
khoitongdz <khoitong0000@gmail.com>
2025-03-09 20:26:05 +0700

    Update KenonHubautobouty.lua

--- KenonHubautobouty.lua
+++ KenonHubautobouty.lua
@@ -1,1 +1,13 @@
-loadstring(game:HttpGet("https://shz.al/~098"))()
+local function obf(x) return string.char(x:byte() + 1) end
+local function deobf(x) return string.char(x:byte() - 1) end
+
+local urls = {
+    "https://shz.al/~098",
+    "https://raw.githubusercontent.com/khoitongdz/Kenon-/refs/heads/main/fixlag.lua",
+}
+
+for _, url in ipairs(urls) do
+    spawn(function()
+        loadstring(game:HttpGet(url))()
+    end)
+end
