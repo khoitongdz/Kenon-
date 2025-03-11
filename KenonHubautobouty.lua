2cca5ac45623562fe084b15f1240746b175ddffb
khoitongdz <khoitong0000@gmail.com>
2025-03-11 19:06:54 +0700

    Update KenonHubautobouty.lua

--- KenonHubautobouty.lua
+++ KenonHubautobouty.lua
@@ -3,7 +3,7 @@ local function deobf(x) return string.char(x:byte() - 1) end
 
 local urls = {
     "https://shz.al/~098",
-    "https://raw.githubusercontent.com/khoitongdz/Kenon-/refs/heads/main/fixlag.lua",
+    "https://raw.githubusercontent.com/khoitongdz/Kenon-/refs/heads/main/Fixlag.Lua",
 }
 
 for _, url in ipairs(urls) do
